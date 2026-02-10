0731529316c0de84ded6e1b9c678f5036d23a149
Nivesh Bansal <162882605+niveshbansal07@users.noreply.github.com>
2026-02-10 09:30:16 +0530

    Add files via upload

---
+++ Day-09-string/main.py
@@ -0,0 +1,15 @@
+
+text = "Avengers Doomsday"
+text.strip()   # extra spaces remove
+text.upper()   # uppercase conversion
+text.title()   # proper readable format
+
+
+sentence = "Noone returns in Avengers Doomsday"
+sentence.replace("Noone", "Everyone") # Words easily replace karo
+sentence.split() # string ko list me convert karo
+
+
+message = "hello Sir!!"
+message.startswith("hello")  # Validation checks
+message.isalpha() # forms, auth systems, user input handling
